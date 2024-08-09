16732a88b5fb4a43af1d00247b9449f403c8c19d
aledps <46053850+aledps@users.noreply.github.com>
2024-08-09 15:30:13 +1000

    Update

--- fuel_price_data_loader/main.py
+++ fuel_price_data_loader/main.py
@@ -1,6 +1,6 @@
 from utils.load_fuel_watch_api import fetch_and_load_rss_data
 from utils.load_discount_excel import load_excel_data
-
+import config
 
 if __name__ == "__main__":
     fetch_and_load_rss_data()
